693df3f3abd1f0001b85fa28d8ea32db27d61671
jahn <jahn@mpusp.mpg.de>
2023-09-01 16:08:24 +0200

    fix: removed unnecessary cat() message

--- R/validate_treemap.R
+++ R/validate_treemap.R
@@ -1,36 +1,36 @@
 #' @importFrom dplyr %>%
 #
 # check input before treemap drawing
-validate_treemap <- function(treemap, 
+validate_treemap <- function(treemap,
   width, height, layout, position, add,
-  levels, color_level, border_level, 
+  levels, color_level, border_level,
   label_level, color_palette,
-  border_color, label_color, 
+  border_color, label_color,
   custom_range, title) {
-  
+
   # check treemap object
   stopifnot(inherits(treemap, c("sunburstResult", "voronoiResult")))
-  
+
   # check layout options
   if (width > 0.9 || height > 0.9) {
     stop("'width' or 'height' should not exceed 0.9.")
   }
-  if ({c(layout, position) > 1} %>% any & !add) {
-    cat("Note: use 'add = TRUE' if you want to add more treemaps to this page.")
+  if (any(layout > 1) & any(position > 1) & !add) {
+    message("Note: use 'add = TRUE' if you want to add more treemaps to this page.")
   }
-  
+
   # check level input
   if (!all(levels %in% 1:length(treemap@call$levels))) {
     stop("Not all indicated 'levels' are contained in this treemap.")
   }
-  
+
   # determine color levels to draw
   if (!is.null(color_level)) {
     if (!all(color_level %in% levels)) {
       stop("The values in 'color_level' must be contained in 'levels'")
     }
   }
-  
+
   if (!is.null(border_level)) {
     if (!all(border_level %in% levels)) {
       stop("The values in 'border_level' must be contained in 'levels'")
@@ -41,14 +41,14 @@ validate_treemap <- function(treemap,
       stop("The values in 'label_level' must be contained in 'levels'")
     }
   }
-  
+
   # check graphical parameter input
   if (!is.null(color_palette)) {
     if (!is.character(color_palette)) {
       stop("'color_palette' must be a character that can be interpreted as colors")
     }
   }
-  
+
   if (!is.null(border_color)) {
     if (!is.character(border_color)) {
       stop("'border_color' must be a character that can be interpreted as colors")
@@ -57,7 +57,7 @@ validate_treemap <- function(treemap,
         stop("'border_color' must be length = 1 or length(border_level)")
     }
   }
-  
+
   if (!is.null(label_color)) {
     if (!is.character(label_color)) {
       stop("'label_color' must be a character that can be interpreted as colors")
@@ -66,18 +66,18 @@ validate_treemap <- function(treemap,
         stop("'label_color' must be length=1 or length(label_level)")
     }
   }
-  
+
   if (!is.null(title)) {
     if (!is.character(title)) {
       stop("'title' must be a character of length 1")
     }
   }
-  
+
   if (!is.null(custom_range)) {
     if (!(is.numeric(custom_range) & length(custom_range) == 2))
       stop("'custom_range' is not a numeric of length 2.")
   }
-  
+
   # check labels
   if (!is.null(label_level)) {
     if (!is.numeric(label_level)) {
@@ -88,5 +88,5 @@ validate_treemap <- function(treemap,
       }
     }
   }
-  
+
 }
