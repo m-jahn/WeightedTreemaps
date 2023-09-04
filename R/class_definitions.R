5b19ac5ef743118f1c74764577e8a54c1d887e5c
jahn <jahn@mpusp.mpg.de>
2023-09-04 15:39:39 +0200

    feature: new methods and utility functions to summarize treemap results objects

--- R/class_definitions.R
+++ R/class_definitions.R
@@ -1,20 +1,19 @@
 #
 # S4 class definitions for sunburst and voronoi treemaps
 # ------------------------------------------------------
-# 
-sunburstResult <- setClass("sunburstResult", 
-  contains = "list", 
+sunburstResult <- setClass("sunburstResult",
+  contains = "list",
   slots = c(
-    cells = "list", 
+    cells = "list",
     data = "data.frame",
     call = "list"
   )
 )
 
-voronoiResult <- setClass("voronoiResult", 
-  contains = "list", 
+voronoiResult <- setClass("voronoiResult",
+  contains = "list",
   slots = c(
-   cells = "list", 
+   cells = "list",
    data = "data.frame",
    call = "list"
   )
@@ -22,4 +21,112 @@ voronoiResult <- setClass("voronoiResult",
 
 # generic methods for these classes
 # ------------------------------------------------------
-# 
+#
+#' as.data.frame.voronoiResult
+#'
+#' Coerces a voronoiResult object to data frame, omitting polygon data.
+#'
+#' @param x (voronoiResult) A voronoi treemap results object
+#' @param ... (none) Not used
+#' @param stringsAsFactors (logical) Transform strings to factors, default FALSE
+#' @return Returns a data.frame
+#' @seealso \code{\link{voronoiTreemap}} for generating the treemap that is
+#'   the input for this function
+#' @export
+as.data.frame.voronoiResult <- function(x, ..., stringsAsFactors = FALSE) {
+  list_cells <- lapply(x@cells, function(y) {
+    y <- y[c("level", "name", "target", "weight", "area", "count", "custom_color")]
+    as.data.frame(y, stringsAsFactors = stringsAsFactors)
+  })
+  do.call(rbind, list_cells)
+}
+
+#' as.data.frame.sunburstResult
+#'
+#' Coerces a sunburstResult object to data frame.
+#'
+#' @param x (sunburstResult) A sunburst treemap results object
+#' @param ... (none) Not used
+#' @param stringsAsFactors (logical) Transform strings to factors, default FALSE
+#' @return Returns a data.frame
+#' @seealso \code{\link{sunburstTreemap}} for generating the treemap that is
+#'   the input for this function
+#' @export
+as.data.frame.sunburstResult <- function(x, ..., stringsAsFactors = FALSE) {
+  list_cells <- lapply(x@cells, function(y) {
+    y <- y[c("level", "name", "area", "lower_bound", "upper_bound", "custom_color")]
+    as.data.frame(y, stringsAsFactors = stringsAsFactors)
+  })
+  do.call(rbind, list_cells)
+}
+
+#' summary.voronoiResult
+#'
+#' Summary method for voronoiResult.
+#'
+#' @param x (voronoiResult) A voronoi treemap results object
+#' @param ... (none) Not used
+#' @return Returns a data.frame
+#' @seealso \code{\link{voronoiTreemap}} for generating the treemap that is
+#'   the input for this function
+#' @importFrom utils head
+#' @export
+summary.voronoiResult <- function(x, ...) {
+  df <- as.data.frame(x)
+  message(paste0(
+    "A treemap object of class 'voronoiResult' with ",
+    nrow(df), " cells on ",
+    length(unique(df$level)), " levels.\n",
+    "The first six cells are:"
+  ))
+  head(df)
+}
+
+#' summary.sunburstResult
+#'
+#' Summary method for sunburstResult
+#'
+#' @param x (sunburstResult) A sunburst treemap results object
+#' @param ... (none) Not used
+#' @return Returns a data.frame
+#' @seealso \code{\link{sunburstTreemap}} for generating the treemap that is
+#'   the input for this function
+#' @export
+summary.sunburstResult <- function(x, ...) {
+  df <- as.data.frame(x)
+  message(paste0(
+    "A treemap object of class 'sunburstResult' with ",
+    nrow(df), " cells on ",
+    length(unique(df$level)), " levels.\n",
+    "The first six cells are:"
+  ))
+  head(df)
+}
+
+#' print.voronoiResult
+#'
+#' Print method for voronoiResult
+#'
+#' @param x (voronoiResult) A voronoi treemap results object
+#' @param ... (none) Not used
+#' @return Returns a data.frame
+#' @seealso \code{\link{voronoiTreemap}} for generating the treemap that is
+#'   the input for this function
+#' @export
+print.voronoiResult <- function(x, ...) {
+  summary(x)
+}
+
+#' print.sunburstResult
+#'
+#' Print method for sunburstResult
+#'
+#' @param x (sunburstResult) A sunburst treemap results object
+#' @param ... (none) Not used
+#' @return Returns a data.frame
+#' @seealso \code{\link{sunburstTreemap}} for generating the treemap that is
+#'   the input for this function
+#' @export
+print.sunburstResult <- function(x, ...) {
+  summary(x)
+}
--- R/polygon_centroids.R
+++ R/polygon_centroids.R
@@ -1,19 +1,19 @@
 #' poly_area
-#' 
-#' Mainly for internal use. Determines the area of a polygon based on 
+#'
+#' Mainly for internal use. Determines the area of a polygon based on
 #' its x and y coordinates.
 #' This function is a reimplementation of `soiltexture::TT.polygon.area()`
 #' and only re-implemented to avoid extra dependencies.
-#' 
+#'
 #' @param poly_x (numeric)  X coordinates of each vertices of the polygon
 #' @param poly_y (numeric)  Y coordinates of each vertices of the polygon
-#' 
+#'
 #' @return A numeric, the area of the polygon.
-#' 
+#'
 #' @seealso soiltexture::TT.polygon.area()
-#' 
+#'
 #' @export
-poly_area <- function (poly_x, poly_y) 
+poly_area <- function (poly_x, poly_y)
 {
   i <- 1:length(poly_x)
   poly_x <- c(poly_x, poly_x[1])
@@ -23,62 +23,80 @@ poly_area <- function (poly_x, poly_y)
 }
 
 #' poly_centroid
-#' 
-#' Mainly for internal use. Determines the centroids of a polygon based on 
+#'
+#' Mainly for internal use. Determines the centroids of a polygon based on
 #' its x and y coordinates.
 #' This function is a reimplementation of `soiltexture::TT.polygon.centroids()`
 #' and only re-implemented to avoid extra dependencies.
-#' 
+#'
 #' @param poly_x (numeric)  X coordinates of each vertices of the polygon
 #' @param poly_y (numeric)  Y coordinates of each vertices of the polygon
-#' 
+#'
 #' @return A numeric, the area of the polygon.
-#' 
+#'
 #' @seealso soiltexture::TT.polygon.centroids()
-#' 
+#'
 #' @export
-poly_centroid <- function(poly_x, poly_y) 
+poly_centroid <- function(poly_x, poly_y)
 {
   i <- 1:length(poly_x)
   A <- poly_area(poly_x, poly_y)
   poly_x <- c(poly_x, poly_x[1])
   poly_y <- c(poly_y, poly_y[1])
-  Cx <- (1/(6 * A)) * sum((poly_x[i] + poly_x[i + 1]) * (poly_x[i] * 
+  Cx <- (1/(6 * A)) * sum((poly_x[i] + poly_x[i + 1]) * (poly_x[i] *
     poly_y[i + 1] - poly_x[i + 1] * poly_y[i]))
-  Cy <- (1/(6 * A)) * sum((poly_y[i] + poly_y[i + 1]) * (poly_x[i] * 
+  Cy <- (1/(6 * A)) * sum((poly_y[i] + poly_y[i + 1]) * (poly_x[i] *
     poly_y[i + 1] - poly_x[i + 1] * poly_y[i]))
   return(c(x = Cx, y = Cy))
 }
 
 #' poly_transform_shape
-#' 
-#' Mainly for internal use. The function transforms ('projects') arbitrary input 
+#'
+#' Mainly for internal use. The function transforms ('projects') arbitrary input
 #' coordinates that are used as parent polygon or shape of the treemap.
 #' By default it scales and centers polygon coordinates on a square
 #' of 0 to 2000 units.
-#' 
+#'
 #' @param poly (list)  named list of 'x' and 'y' coordinates of the polygon
 #' @param xy_max (numeric)  maximum desired expansion of the polygon (default: 2000)
-#' 
+#'
 #' @return A list with slots 'x' and 'y' containing numeric coordinates
-#' 
+#'
 #' @export
 poly_transform_shape <- function(poly, xy_max = 2000) {
-  
+
   # check input list
   if(!all(names(poly) %in% c("x", "y"))) {
     stop("Supplied list for 'shape' does not contain 'x' and 'y' slots")
   }
-  
+
   # determine the range for X and Y direction
   range <- list(x = range(poly$x), y = range(poly$y))
   large_side <- ifelse(diff(range$x) >= diff(range$y), "x", "y")
   small_side <- switch(large_side, x = "y", y = "x")
-  
+
   # rescale and shift to center
   scale_factor <- xy_max/diff(range[[large_side]])
   poly[[large_side]] <- (poly[[large_side]]-min(range[[large_side]]))*scale_factor
   poly[[small_side]] <- (poly[[small_side]]-min(range[[small_side]]))*scale_factor
   poly[[small_side]] <- poly[[small_side]]+(xy_max-max(poly[[small_side]]))/2
   return(poly)
+}
+
+#' get_polygons
+#'
+#' Extract the list of polygons from a voronoiResult object.
+#'
+#' @param x (voronoiResult) A voronoi treemap results object
+#' @param ... (none) Not used
+#' @return List of polygons, as class POLYGON from sf
+#' @seealso \code{\link{voronoiTreemap}} for generating the treemap that is
+#'   the input for this function
+#' @export
+get_polygons <- function(x, ...) {
+  list_polys <- lapply(x@cells, function(y) {
+    poly <- y$poly
+  })
+  names(list_polys) <- names(x@cells)
+  list_polys
 }
